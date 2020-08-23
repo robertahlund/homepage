2a2c05119a1c2e6b020fafb89fcd721a48730514
robertahlund <osg-wn3-7kf-knV>
2020-08-23 21:05:15 +0200

    Updates

---
+++ precache-manifest.46d6c30d470d5c4425566b1aac97142f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a7f26758ce1dea75bd25283c0873d5",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "8123093577bf739827ae",
+    "url": "/homepage/static/css/main.2098c847.chunk.css"
+  },
+  {
+    "revision": "faa87341f11fc6141182",
+    "url": "/homepage/static/js/2.0289b573.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/homepage/static/js/2.0289b573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8123093577bf739827ae",
+    "url": "/homepage/static/js/main.900dbeec.chunk.js"
+  },
+  {
+    "revision": "836f55bc99e549bdd710",
+    "url": "/homepage/static/js/runtime-main.3ed86e83.js"
+  },
+  {
+    "revision": "2fe9a2bdaa6daf61e770a2edb7252040",
+    "url": "/homepage/static/media/JetBrainsMono-Regular.2fe9a2bd.woff"
+  }
+]);
